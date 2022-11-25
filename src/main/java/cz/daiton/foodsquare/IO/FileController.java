c8f951adc2d6d72594cd23296d92f9ef4a7ee54b
David Poslušný <david.poslusny@email.cz>
2022-11-25 15:56:28 +0100

    docs for file controller

--- src/main/java/cz/daiton/foodsquare/IO/FileController.java
+++ src/main/java/cz/daiton/foodsquare/IO/FileController.java
@@ -1,6 +1,9 @@
 package cz.daiton.foodsquare.IO;
 
 import cz.daiton.foodsquare.payload.response.MessageResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StreamUtils;
@@ -19,12 +22,16 @@ import java.io.InputStream;
         allowCredentials = "true"
 )
 @AllArgsConstructor
+@Tag(description = "Set of endpoints returning specific images.", name = "File Controller")
 public class FileController {
 
     private final FileStorageService fileStorageService;
 
     @GetMapping(value = "/recipes/{filename}")
-    public void getRecipeImage(@PathVariable String filename, HttpServletResponse response) throws IOException {
+    @Operation(summary = "Returns a recipe image based on given parameter.")
+    public void getRecipeImage(
+            @Parameter(description = "Path to image.", example = "1.jpg") @PathVariable String filename,
+            HttpServletResponse response) throws IOException {
         InputStream img = fileStorageService.load(filename, "recipe");
         String contentType = fileStorageService.getContentType(filename, "recipe");
         response.setContentType(contentType);
@@ -33,7 +40,10 @@ public class FileController {
     }
 
     @GetMapping(value = "/users/{filename}")
-    public void getUserImage(@PathVariable String filename, HttpServletResponse response) throws IOException {
+    @Operation(summary = "Returns a user profile picture based on given parameter.")
+    public void getUserImage(
+            @Parameter(description = "Path to image.", example = "1.png") @PathVariable String filename,
+            HttpServletResponse response) throws IOException {
         InputStream img = fileStorageService.load(filename, "user");
         String contentType = fileStorageService.getContentType(filename, "user");
         response.setContentType(contentType);
