5d05bec85ca1d4bd5a042367649887df55051b39
David Poslušný <david.poslusny@email.cz>
2022-11-25 17:50:13 +0100

    docs for review

--- src/main/java/cz/daiton/foodsquare/review/ReviewController.java
+++ src/main/java/cz/daiton/foodsquare/review/ReviewController.java
@@ -2,6 +2,9 @@ package cz.daiton.foodsquare.review;
 
 import cz.daiton.foodsquare.exceptions.IncorrectUserException;
 import cz.daiton.foodsquare.payload.response.MessageResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
@@ -26,86 +29,121 @@ import java.util.NoSuchElementException;
         allowCredentials = "true"
 )
 @AllArgsConstructor
+@Tag(description = "Set of endpoints for CRUD operations with reviews.", name = "Review Controller")
 public class ReviewController {
 
     private final ReviewService reviewService;
 
     @GetMapping(value = "{id}")
-    public Review getReview(@PathVariable Long id) {
+    @Operation(summary = "Returns a specific review based on given parameter.")
+    public Review getReview(@Parameter(description = "ID of the review.", example = "1") @PathVariable Long id) {
         return reviewService.get(id);
     }
 
     @GetMapping()
+    @Operation(summary = "Returns list of all reviews in the application.")
     public List<Review> getReviews() {
         return reviewService.getAll();
     }
 
     @GetMapping(value = "/recipe/{id}/my-review")
     @PreAuthorize("isAuthenticated()")
-    public Review getReviewByRecipe(@PathVariable Long id, HttpServletRequest request) throws Exception {
+    @Operation(summary = "Returns a review for a specific user on a recipe.")
+    public Review getReviewByRecipe(
+            @Parameter(description = "ID of the recipe.", example = "1") @PathVariable Long id,
+            HttpServletRequest request) throws Exception {
         return reviewService.getByRecipe(id, request);
     }
 
     @GetMapping(value = "/user/{id}")
-    public List<Review> getAllByAppUser(@PathVariable Long id) {
+    @Operation(summary = "Returns all reviews for a specific user.")
+    public List<Review> getAllByAppUser(
+            @Parameter(description = "ID of the user.", example = "1") @PathVariable Long id) {
         return reviewService.getAllByAppUser(id);
     }
 
     @GetMapping(value = "/{id}/likes")
-    public Integer getReviewLikes(@PathVariable Long id) {
+    @Operation(summary = "Returns a count of likes of a specific review.")
+    public Integer getReviewLikes(
+            @Parameter(description = "ID of the review.", example = "1") @PathVariable Long id) {
         return reviewService.countLikes(id);
     }
 
     @GetMapping(value = "/{id}/like-check")
     @PreAuthorize("isAuthenticated()")
-    public Boolean isLikedByUser(@PathVariable Long id, HttpServletRequest request) throws Exception {
+    @Operation(summary = "Checks whether user has liked a specific review.")
+    public Boolean isLikedByUser(
+            @Parameter(description = "ID of the review.", example = "1") @PathVariable Long id,
+            HttpServletRequest request) throws Exception {
         return reviewService.isLikedByUser(id, request);
     }
 
     @GetMapping(value = "/recipe/{id}")
-    public List<Review> getAllByRecipe(@PathVariable Long id) {
+    @Operation(summary = "Returns all reviews of a specific review.")
+    public List<Review> getAllByRecipe(
+            @Parameter(description = "ID of the recipe.", example = "1") @PathVariable Long id) {
         return reviewService.getAllByRecipe(id);
     }
 
     @GetMapping(value = "/recipe/{id}/count")
-    public Integer countReviewsByRecipe(@PathVariable Long id) {
+    @Operation(summary = "Returns a count of all reviews of a recipe.")
+    public Integer countReviewsByRecipe(
+            @Parameter(description = "ID of the recipe.", example = "1") @PathVariable Long id) {
         return reviewService.countByRecipe(id);
     }
 
     @GetMapping(value = "/recipe/{id}/avg-rating")
-    public BigDecimal getAvgRatingOfRecipe(@PathVariable Long id) {
+    @Operation(summary = "Returns a average review rating for a recipe.")
+    public BigDecimal getAvgRatingOfRecipe(
+            @Parameter(description = "ID of the recipe.", example = "1") @PathVariable Long id) {
         return reviewService.getAverageRating(id);
     }
 
     @GetMapping(value = "/recipe/{id}/my-review-check")
     @PreAuthorize("isAuthenticated()")
-    public Boolean containsReview(@PathVariable Long id, HttpServletRequest request) throws Exception {
+    @Operation(summary = "Checks whether user has reviewed a specific recipe.")
+    public Boolean containsReview(
+            @Parameter(description = "ID of the recipe.", example = "1") @PathVariable Long id,
+            HttpServletRequest request) throws Exception {
         return reviewService.containsReview(id, request);
     }
 
     @PostMapping(value = "/add")
-    public ResponseEntity<?> addReview(@Valid @RequestBody ReviewDto reviewDto, HttpServletRequest request) throws Exception {
+    @Operation(summary = "Adds a review.")
+    public ResponseEntity<?> addReview(
+            @Valid @RequestBody ReviewDto reviewDto,
+            HttpServletRequest request) throws Exception {
         return ResponseEntity
                 .ok()
                 .body(new MessageResponse(reviewService.add(reviewDto, request)));
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<?> updateReview(@Valid @RequestBody ReviewDto reviewDto, @PathVariable Long id, HttpServletRequest request) throws Exception {
+    @Operation(summary = "Updates a review.")
+    public ResponseEntity<?> updateReview(
+            @Valid @RequestBody ReviewDto reviewDto,
+            @Parameter(description = "ID of the review.", example = "1") @PathVariable Long id,
+            HttpServletRequest request) throws Exception {
         return ResponseEntity
                 .ok()
                 .body(new MessageResponse(reviewService.update(reviewDto, id, request)));
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<?> deleteReview(@PathVariable Long id, HttpServletRequest request) throws Exception {
+    @Operation(summary = "Deletes a review.")
+    public ResponseEntity<?> deleteReview(
+            @Parameter(description = "ID of the review.", example = "1") @PathVariable Long id,
+            HttpServletRequest request) throws Exception {
         return ResponseEntity
                 .ok()
                 .body(new MessageResponse(reviewService.delete(id, request)));
     }
 
     @DeleteMapping(value = "/user/{id}")
-    public ResponseEntity<?> deleteReviewByRecipeAndAppUser(@PathVariable Long id, HttpServletRequest request) throws Exception {
+    @Operation(summary = "Deletes a review for a specific user from a specific recipe.")
+    public ResponseEntity<?> deleteReviewByRecipeAndAppUser(
+            @Parameter(description = "ID of the recipe.", example = "1") @PathVariable Long id,
+            HttpServletRequest request) throws Exception {
         return ResponseEntity
                 .ok()
                 .body(new MessageResponse(reviewService.deleteByRecipe(id, request)));
