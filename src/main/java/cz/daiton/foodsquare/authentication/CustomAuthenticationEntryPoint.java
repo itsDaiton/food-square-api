d91b9e9aa2469a4f555cd6cc3f585354d03b52ba
David Poslušný <david.poslusny@email.cz>
2022-12-07 18:54:36 +0100

    error msg localization fix

--- src/main/java/cz/daiton/foodsquare/authentication/CustomAuthenticationEntryPoint.java
+++ src/main/java/cz/daiton/foodsquare/authentication/CustomAuthenticationEntryPoint.java
@@ -33,7 +33,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
         final Map<String, Object> body = new HashMap<>();
         body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
         body.put("error", "Unauthorized");
-        body.put("message", authException.getMessage() + ".");
+        body.put("message", authException.getLocalizedMessage() + ".");
         body.put("path", request.getServletPath());
 
         final ObjectMapper mapper = new ObjectMapper();
